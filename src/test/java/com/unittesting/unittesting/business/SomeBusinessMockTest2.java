c1073820df46f46edc237088dc32cdc7215e9600
dsirit <david.sirit@globalincubator.com>
2019-09-24 10:22:57 +0200

    using mockito annotations

--- src/test/java/com/unittesting/unittesting/business/SomeBusinessMockTest2.java
+++ src/test/java/com/unittesting/unittesting/business/SomeBusinessMockTest2.java
@@ -6,26 +6,40 @@ import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import business.SomeBusinessImpl;
 import data.SomeDataService;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessMockTest2 {
 	
+	// esto se puede mejorar con @InjectMocks y @Mock
+	// luego no haria falta hacer uso del @Before en este caso
+	// Mockito hace la instancia de lo que se inyecta
+	/*
 	SomeBusinessImpl business = new SomeBusinessImpl();
 	SomeDataService dataServiceMock = mock(SomeDataService.class);		
 	
 	@Before
 	public void beforeTest() {
 		business.setSomeDataService(dataServiceMock);
-	}
+	}*/	
 	
+	@InjectMocks
+	SomeBusinessImpl business;  // no se crea la instancia, solo de declara
+	
+	@Mock
+	SomeDataService dataServiceMock;
 
 	@Test
 	public void calculateSumUsingSomeDataService_basic() {
 		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2, 3, 0 });			
 		assertEquals(6, business.calculateSumUsingSomeDataService());
-
+		
 	}
 
 	@Test
