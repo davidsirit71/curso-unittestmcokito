1ea210a8b229d262f6ff88b26b36448afbbbcc6e
dsirit <david.sirit@globalincubator.com>
2019-09-24 10:05:24 +0200

    refactor first mock test

--- src/test/java/com/unittesting/unittesting/business/SomeBusinessMockTest2.java
+++ src/test/java/com/unittesting/unittesting/business/SomeBusinessMockTest2.java
@@ -1,52 +1,44 @@
 package com.unittesting.unittesting.business;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import business.SomeBusinessImpl;
 import data.SomeDataService;
 
 public class SomeBusinessMockTest2 {
+	
+	SomeBusinessImpl business = new SomeBusinessImpl();
+	SomeDataService dataServiceMock = mock(SomeDataService.class);		
+	
+	@Before
+	public void beforeTest() {
+		business.setSomeDataService(dataServiceMock);
+	}
+	
 
 	@Test
 	public void calculateSumUsingSomeDataService_basic() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock = mock(SomeDataService.class);		
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2, 3, 0 });	
-		
-		business.setSomeDataService(dataServiceMock);
-		int actualResult = business.calculateSumUsingSomeDataService();
-		int expectedResult = 6;
-		assertEquals(expectedResult, actualResult);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2, 3, 0 });			
+		assertEquals(6, business.calculateSumUsingSomeDataService());
 
 	}
 
 	@Test
 	public void calculateSumUsingSomeDataService_empty() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock = mock(SomeDataService.class);		
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { });	
-		
-		business.setSomeDataService(dataServiceMock);
-		int actualResult = business.calculateSumUsingSomeDataService();
-		int expectedResult = 0;
-		assertEquals(expectedResult, actualResult);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { });			
+		assertEquals(0, business.calculateSumUsingSomeDataService());
 
 	}
 
 	@Test
 	public void calculateSumUsingSomeDataService_oneValue() {
-		SomeBusinessImpl business = new SomeBusinessImpl();
-		SomeDataService dataServiceMock = mock(SomeDataService.class);		
-		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { -3});	
-		
-		business.setSomeDataService(dataServiceMock);
-		int actualResult = business.calculateSumUsingSomeDataService();
-		int expectedResult = -3;
-		assertEquals(expectedResult, actualResult);
+		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { -3});			
+		assertEquals(-3, business.calculateSumUsingSomeDataService());
 
 	}
 
