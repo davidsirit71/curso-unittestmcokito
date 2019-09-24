05ca2dcb6e1a4ce0eb7168c89e8f70879bb8f7f6
dsirit <david.sirit@globalincubator.com>
2019-09-24 11:37:43 +0200

    using verifications

--- src/test/java/com/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/unittesting/unittesting/business/ListMockTest.java
@@ -5,13 +5,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
 
 import java.util.List;
 
 public class ListMockTest {
 
-	List mock = mock(List.class);		
+	List<String> mock = mock(List.class);		
 	
 	@Test
 	public void size_basic() {
@@ -31,6 +38,32 @@ public class ListMockTest {
 		when(mock.get(0)).thenReturn("texto de prueba");
 		assertEquals("texto de prueba", mock.get(0));
 		assertEquals(null, mock.get(1));		// este test pasa porque retorna el valor por defecto que es null
+	}
+	
+	@Test
+	public void returnWithGenericParameters() {
+		when(mock.get(anyInt())).thenReturn("texto de prueba").thenReturn("iniciales");
+		assertEquals("texto de prueba", mock.get(0));
+		assertEquals("iniciales", mock.get(1));
+	}
+	
+	@Test
+	public void verificationBasics() {
+		
+		//SUT
+		String value1 = mock.get(0);
+		String value2 = mock.get(1);
+		
+		//Verify  /* se usa para verificar si un metodo se llama sin importar el valor */
+		verify(mock).get(0);
+		//verify(mock).get(anyInt());				// falla porque se llama 2 veces
+		//verify(mock, times(1)).get(anyInt());  // falla porque se llama 2 veces
+		verify(mock, times(2)).get(anyInt());
+		verify(mock, atLeast(1)).get(anyInt());
+		verify(mock, atLeastOnce()).get(anyInt());
+		verify(mock, atMost(2)).get(anyInt());
+		verify(mock, never()).get(2);
+		
 	}
 
 }
