01912ea72b700399a895950990a75b11d7c1dc3e
dsirit <david.sirit@globalincubator.com>
2019-09-24 13:14:52 +0200

    using spy and difference with mock

--- src/test/java/com/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/unittesting/unittesting/business/ListMockTest.java
@@ -7,6 +7,7 @@ import org.mockito.ArgumentCaptor;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,6 +16,7 @@ import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListMockTest {
@@ -96,7 +98,44 @@ public class ListMockTest {
 		List<String> allValues = captor.getAllValues();
 		assertEquals("SomeString1", allValues.get(0));
 		assertEquals("SomeString2", allValues.get(1));
-
 		
 	}
+	
+	@Test
+	public void noRealSpaying() {
+		ArrayList<String> arrayListMock = mock(ArrayList.class);
+		System.out.println(arrayListMock.get(0)); // se obtiene null
+		System.out.println(arrayListMock.size()); // se obtine 0
+		System.out.println(arrayListMock.add("Test1")); // en consola es falso
+		System.out.println(arrayListMock.add("Test2")); // en consola es falso
+		System.out.println(arrayListMock.size()); // se obtiene 0 porque es mock
+		
+		when(arrayListMock.size()).thenReturn(5);
+		System.out.println(arrayListMock.size()); // ya retorna 5 por la linea de arriba
+		
+	}
+	
+	@Test
+	public void spaying() {
+		ArrayList<String> arrayListSpy = spy(ArrayList.class);
+		arrayListSpy.add(0, "Test0");
+		
+		System.out.println(arrayListSpy.get(0)); // Test0
+		System.out.println(arrayListSpy.size()); // 1
+		System.out.println(arrayListSpy.add("Test1")); //true 
+		System.out.println(arrayListSpy.add("Test2")); //true 
+		System.out.println(arrayListSpy.size()); // 3
+		
+		when(arrayListSpy.size()).thenReturn(5); 
+		System.out.println(arrayListSpy.size()); // 5
+		
+		arrayListSpy.add("Test4");
+		System.out.println(arrayListSpy.size()); // 5
+		
+		verify(arrayListSpy).add("Test4");
+		
+		
+		
+	}
+	
 }
